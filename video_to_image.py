4e1f6e0df01d064ccf9d9823d878acc3698d8fa2
ARATAYOKOYAMA <bob2525@hotmail.co.jp>
2019-08-02 19:46:50 +0900

    準備完了

---
+++ video_to_image.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+
+def save_all_frames(video_path, dir_path, basename, ext='jpg'):
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        return
+
+    os.makedirs(dir_path, exist_ok=True)
+    base_path = os.path.join(dir_path, basename)
+
+    digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
+
+    n = 0
+
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            cv2.imwrite('{}-{}.{}'.format(base_path, str(n).zfill(digit), ext), frame)
+            n += 1
+        else:
+            return
+
+# How to
+# save_all_frames('data/temp/sample_video.mp4', 'data/temp/result', 'sample_video_img')
+save_all_frames('video/output_1_a.mp4', 'images/001', '1-video', 'jpg')
